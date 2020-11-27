e4be3ea1cae530f25fa76a291d2454350e68f749
MarcMerino <marcmerino24@gmail.com>
2020-11-27 21:20:25 +0100

    Hibernate 1.0 Classpath fixed

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,24 @@
+import implementations.UserImpl;
+import interfaces.IUser;
+import models.Exercise;
+import models.User;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// Test
+		User u1 = new User("test", "1234", 0, "", 1, "test@duolingo.com");
+		
+		Exercise e1 = new Exercise("Translate", 5, null);
+		Exercise e2 = new Exercise("Listen", 10, null);
+		
+		u1.addExercise(e1);
+		u1.addExercise(e2);
+		
+		IUser userManager = new UserImpl();
+		
+		userManager.insertUser(u1);
+		
+		
 	}
 
 }
