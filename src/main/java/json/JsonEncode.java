4c53448daee9e3db428786591eac91b627bcbdc4
MarcMerino <marcmerino24@gmail.com>
2020-12-17 21:34:52 +0100

    Added method in JsonEncode.java

--- src/main/java/json/JsonEncode.java
+++ src/main/java/json/JsonEncode.java
@@ -3,9 +3,10 @@ package json;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+@SuppressWarnings("unchecked")
 public class JsonEncode {
 
-	@SuppressWarnings("unchecked")
+	
 	public static String jsonContentInsertTest(String exercise_type, int exerciseExp, int exerciseCoins, String question, String correct_answer, String answers) {
 		
 		String content;
@@ -30,7 +31,32 @@ public class JsonEncode {
 			return content;
 		}
 		
-		// Futuros ELSE IF para crear una condicion para cada tipo de ejercicio
+		return null;
+	}
+
+	public static String jsonContentInsertOpenTraduct(String exercise_type, int exerciseExp, int exerciseCoins, String question, String[] answers) {
+		
+		String content;
+
+		JSONObject objectManager = new JSONObject();
+		JSONArray objectArray = new JSONArray();
+		
+		if (exercise_type.equals("TRADUCCIO_OBERTA")) {
+			objectManager.put("exerciseType", exercise_type);
+			objectManager.put("exerciseExp", exerciseExp);
+			objectManager.put("exerciseCoins", exerciseCoins);
+			objectManager.put("sentenceToTranslate", question);
+			
+			
+			for (int i = 0; i < answers.length; i++) {
+				objectArray.add(answers[i]);
+			}
+			objectManager.put("traductedSentences", objectArray);
+			
+			content = objectManager.toString();
+			return content;
+			
+		}
 		
 		return null;
 	}
